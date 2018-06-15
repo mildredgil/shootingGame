c5f4d614cfc12fa737f5e0a46fc6bf15e99739f4
Mildred <mi.mitel@hotmail.com>
2018-06-14 19:37:03 -0500

    background, bar, ball

---
+++ New Unity Project (2)/Assets/bar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bar : MonoBehaviour {
+  public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		  transform.Translate(0f,-moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f);
+	}
+}
---
+++ New Unity Project (2)/Assets/shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+  public Rigidbody bullet;
+  Rigidbody clone;
+  float speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown ("Fire1")) {
+      clone = Instantiate(bullet, transform.position, transform.rotation);
+  		clone.velocity = transform.TransformDirection( new Vector3(0, 0, -speed));
+  		Destroy (clone.gameObject, 1);
+    }
+    
+	}
+}
