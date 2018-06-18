2eff741e991e53b437d52b27b67e1f63398386d3
Marcela Rico <marce.rr@hotmail.com>
2018-06-17 21:34:15 -0500

    About changes marce

---
+++ New Unity Project (2)/Assets/changeCont.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeCont : MonoBehaviour
+{
+
+    public void ChangeCont(string scene)
+    {
+        SceneManager.LoadScene("About");
+    }
+}
+
