b6d231c94f633227075ff0b40246336aa124da2d
Mildred <mi.mitel@hotmail.com>
2018-06-17 21:44:10 -0500

    shoot the ball

---
+++ New Unity Project (2)/Assets/scripts/bar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+public class bar : MonoBehaviour {
+  public float moveSpeed;
+  public GameObject bullet;
+  public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		  transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,0f);
+
+      if(Input.GetKeyDown("space")) {
+        GameObject bulletpos = (GameObject)Instantiate (Player);
+        bulletpos.transform.position = bullet.transform.position;
+      }
+	}
+}
+  
---
+++ New Unity Project (2)/Assets/scripts/shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+  float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector2 position = transform.position;
+    position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+    Debug.Log(position.y);
+    transform.position = position;
+
+    Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1 ));
+
+    if(transform.position.y > max.y){
+      Destroy(gameObject);
+    }
+
+		if(Input.GetButtonDown ("Fire1")) {
+      
+      /*clone = Instantiate(bullet, transform.position, transform.rotation);
+  		clone.velocity = transform.TransformDirection( new Vector2(0, speed));
+  		Destroy (clone.gameObject, 5);*/
+    }
+	}
+}
